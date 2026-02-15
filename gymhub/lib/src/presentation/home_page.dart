2b181712855b9350c1ae671632078c717db0eb43
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2026-02-14 22:04:31 -0600

    Added JSON file and button to add new exercise to the JSON file

---
+++ gymhub/lib/src/presentation/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../data/local_file_service.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final LocalFileService fileService = LocalFileService();
+  String jsonContent = '';
+
+  @override
+  void initState() {
+    super.initState();
+    initFile();
+  }
+
+  Future<void> initFile() async {
+    await fileService.ensureExercisesFileExists();
+  }
+
+  Future<void> loadJson() async {
+    final data = await fileService.readExercises();
+    setState(() {
+      jsonContent = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('GymHub – Catálogo editable')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: loadJson,
+              child: Text('Leer exercises.json'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(jsonContent),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
+    );
+  }
+}
